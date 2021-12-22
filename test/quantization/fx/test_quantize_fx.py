e6060e6c6674e1c8127ef09852252bce7b4212fb
Jerry Zhang <jerryzh168@gmail.com>
2021-12-21 19:16:20 -0800

    [bc-breaking][quant][be] Refactor fuser_method to include `is_qat` argument
    Summary:
    Currently we rely on module.training to decide whether we'll do a qat fusion or ptq fusion, this is
    not ideal since training flag has nothing to do with quantization, this PR introduces an extra flag `is_qat`
    to control this
    
    Note: currently we still has the constraint that when `is_qat` is True, the modules must be in training mode, we
    can relax this constraint later
    
    Test Plan:
    ```
    python test/test_quantization.py TestFuseFx
    python test/test_quantization.py TestFusion
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 0fefaba0cc63343392a9ede335882dc9d7a4d532
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/70009

--- test/quantization/fx/test_quantize_fx.py
+++ test/quantization/fx/test_quantize_fx.py
@@ -364,6 +364,8 @@ class TestFuseFx(QuantizationTestCase):
 
     @skipIfNoFBGEMM
     def test_qconfig_fused_module(self):
+        """ TODO: add test for all fused modules
+        """
         qconfig_dict = {
             "": None,
             "object_type": [(nn.Linear, default_qconfig),
@@ -854,15 +856,17 @@ class TestQuantizeFx(QuantizationTestCase):
                 m_eager.eval()
                 qconfig = get_default_qconfig(qengine)
                 prepare_fn = prepare
+                is_qat = False
             else:
                 m_eager.train()
                 qconfig = get_default_qat_qconfig(qengine)
                 prepare_fn = prepare_qat
+                is_qat = True
 
             fuse_list = ["conv", "bn"]
             if has_relu:
                 fuse_list.append("relu")
-            fuse_modules(m_eager, fuse_list, inplace=True)
+            fuse_modules(m_eager, fuse_list, inplace=True, is_qat=is_qat)
             m_eager.qconfig = qconfig
             m_eager = prepare_fn(m_eager)
             m_eager(*self.img_data_dict[dim][0])
@@ -5751,6 +5755,7 @@ class TestQuantizeFxModels(QuantizationTestCase):
             graph.eval()
             calibrate_or_train = test_only_eval_fn
             data = self.img_data_2d
+            is_qat = False
         else:
             assert quant_type == QuantType.QAT
             qconfig = default_qat_qconfig
@@ -5760,9 +5765,10 @@ class TestQuantizeFxModels(QuantizationTestCase):
             graph.train()
             calibrate_or_train = test_only_train_fn
             data = self.img_data_2d_train
+            is_qat = True
 
         if hasattr(eager, "fuse_model"):
-            eager.fuse_model()
+            eager.fuse_model(is_qat)
         eager = QuantWrapper(eager)
         eager.qconfig = qconfig
         eager = eager_prepare(eager)
